d118e05259217a36244312138a64863eb5be64de
na-young-kwon <kny0651@naver.com>
2024-05-10 21:50:06 +0900

    메모 추가 기능 구현

--- AnyTodo/any_todo/lib/view/MainView.dart
+++ AnyTodo/any_todo/lib/view/MainView.dart
@@ -1,7 +1,7 @@
-
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
@@ -11,18 +11,75 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  final List<String> todos = ['머리감기', '여행가기'];
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.8;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text('ALL TASKS', style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        ),),
+        title: const Text(
+          'ALL TASKS',
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.white),
+            onPressed: () {},
+            child: const Text('메뉴', style: TextStyle(fontSize: 20)),
+          ),
+        ],
       ),
-      body: const Text('머리감기')
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text('오늘',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold)),
+                  Text('+', style: TextStyle(color: Colors.white))
+                ])),
+        ListView.builder(
+          shrinkWrap: true,
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.all(10),
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              width: width,
+              height: 50,
+              child: Text(
+                todos[index],
+                style: const TextStyle(
+                    backgroundColor: Colors.orange, color: Colors.white),
+              ),
+            );
+          },
+        ),
+        TextField(
+          style: const TextStyle(color: Colors.white),
+          controller: _textController,
+          onSubmitted: addTodo,
+          decoration: const InputDecoration(
+              labelText: '텍스트 입력',
+              hintText: '텍스트를 입력해주세요',
+              border: OutlineInputBorder()),
+        )
+      ]),
     );
+  }
+
+  void addTodo(String text) {
+    _textController.clear();
+    todos.add(text);
+    setState(() {});
   }
 }
